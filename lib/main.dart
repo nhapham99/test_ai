610b0f3dccab50fc9f50164f537cb035d96b4c7c
BnBStudio <bnbstudio@BnBStudios-Mac-mini.local>
2024-11-10 21:18:21 +0700

    run in isolate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
+import 'dart:isolate';
+import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:opencv_dart/opencv.dart' as cv;
@@ -40,13 +43,114 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Rect {
-  int x;
-  int y;
-  int width;
-  int height;
+int runTask(List<dynamic> args) {
+  SendPort resultPort = args[0];
 
-  Rect(this.x, this.y, this.width, this.height);
+  final reuslt = processImage(args[1]);
+
+  Isolate.exit(resultPort, reuslt);
+}
+
+(int, int, int, int, int, int) processImage(String imagePath) {
+  final image = cv.imread(imagePath, flags: cv.IMREAD_GRAYSCALE);
+  final clahe = cv.createCLAHE(clipLimit: 2.0, tileGridSize: (8, 8));
+  final equalizedImg = clahe.apply(image);
+  final blurred = cv.gaussianBlur(equalizedImg, (5, 5), 0);
+  final sharpened = cv.addWeighted(equalizedImg, 1.5, blurred, -0.5, 0);
+  final thresh =
+      cv.threshold(sharpened, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU).$2;
+
+  // Remove horizontal lines using morphological opening
+  final lineKernel = cv.getStructuringElement(cv.MORPH_RECT, (20, 1));
+  final linesRemoved =
+      cv.morphologyEx(thresh, cv.MORPH_OPEN, lineKernel, iterations: 1);
+
+  // Subtract horizontal line from image
+  final textOnly = cv.subtract(thresh, linesRemoved);
+
+  // Apply morphological opening to clean noise
+  final openingKernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3));
+  final opening =
+      cv.morphologyEx(textOnly, cv.MORPH_OPEN, openingKernel, iterations: 1);
+
+  // Dilate to connect text elements
+  final smallKernel = cv.getStructuringElement(cv.MORPH_RECT, (10, 30));
+  final smallDilate = cv.dilate(opening, smallKernel, iterations: 1);
+
+  final mediumKernel = cv.getStructuringElement(cv.MORPH_RECT, (30, 50));
+  final mediumDilate = cv.dilate(smallDilate, mediumKernel, iterations: 1);
+
+  final largeKernel = cv.getStructuringElement(cv.MORPH_RECT, (50, 70));
+  final dilate = cv.dilate(mediumDilate, largeKernel, iterations: 1);
+
+  // Remove remaining unwanted contours (e.g., specific shapes or areas)
+  var cnts =
+      cv.findContours(dilate, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE).$1;
+  for (final c in cnts) {
+    final area = cv.contourArea(c);
+    final rect = cv.boundingRect(c);
+    final ar = rect.width / rect.height;
+    if (area > 10000 && area < 12500 && ar < 0.5) {
+      cv.drawContours(dilate, cv.VecVecPoint.fromVecPoint(c), -1, cv.Scalar(),
+          thickness: -1);
+    }
+  }
+
+  var finalKernelRect = (6, 6);
+  var finalInterations = 5;
+
+  if (cnts.length < 10) {
+    finalKernelRect = (100, 100);
+    finalInterations = 2;
+  }
+
+  // Additional dilation to enhance detected regions
+  final finalKernel = cv.getStructuringElement(cv.MORPH_RECT, finalKernelRect);
+  final finalDilate = cv.dilate(
+    dilate,
+    finalKernel,
+    iterations: finalInterations,
+  );
+
+  // Draw bounding boxes around large contours
+  final finalContours =
+      cv.findContours(finalDilate, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE).$1;
+
+  // Calculate the center of the image
+  final imageCenter = cv.Point(image.cols ~/ 2, image.rows ~/ 2);
+
+  cv.Rect? biggestNearestRect;
+  double maxArea = 0;
+  double minDistance = double.infinity;
+
+  for (final c in finalContours) {
+    final area = cv.contourArea(c);
+    if (area > 100000) {
+      final rect = cv.boundingRect(c);
+      final rectCenter =
+          cv.Point(rect.x + rect.width ~/ 2, rect.y + rect.height ~/ 2);
+
+      // Calculate Euclidean distance manually
+      final dx = imageCenter.x - rectCenter.x;
+      final dy = imageCenter.y - rectCenter.y;
+      final distance = sqrt(dx * dx + dy * dy);
+
+      // Check if this rectangle is bigger and closer than previously found
+      if (area > maxArea && distance < minDistance) {
+        maxArea = area;
+        minDistance = distance;
+        biggestNearestRect = rect;
+      }
+    }
+  }
+  return (
+    biggestNearestRect?.x ?? 0,
+    biggestNearestRect?.y ?? 0,
+    biggestNearestRect?.width ?? image.cols,
+    biggestNearestRect?.height ?? image.rows,
+    image.cols,
+    image.rows,
+  );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -71,8 +175,7 @@ class _MyHomePageState extends State<MyHomePage> {
   File? _image;
   List<Rect> _contours = [];
   Uint8List? _stitchedImage;
-  int imageHeight = 0;
-  int imageWidth = 0;
+  final ReceivePort receivePort = ReceivePort();
 
   Future<void> _pickImage() async {
     setState(() {
@@ -86,49 +189,30 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         _image = File(pickedFile.path);
       });
-      _processImage(_image!);
+      final DateTime startTime = DateTime.now();
+      print("Start processing at: $startTime");
+      String path = pickedFile.path;
+      isolateFunc(path);
     }
   }
 
-  Future<void> _processImage(File imageFile) async {
-    final mat = cv.imread(imageFile.path);
-
-    final ratio = mat.width / MediaQuery.of(context).size.width;
-    setState(() {
-      imageHeight = (mat.height / ratio).toInt();
-      imageWidth = MediaQuery.of(context).size.width.toInt();
-    });
-
-    final gray = cv.cvtColor(mat, cv.COLOR_BGR2GRAY);
-    final blur = cv.medianBlur(gray, 5);
-    final thresh = cv.adaptiveThreshold(
-        blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 14);
-
-    final kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5));
-    final dilate = cv.dilate(thresh, kernel, iterations: 50);
-    final (success, bytes) = await cv.imencodeAsync(".png", dilate);
-
-    setState(() {
-      _stitchedImage = bytes;
-    });
-    if (!success) {
-      throw Exception("Failed to encode image");
+  void isolateFunc(String path) async {
+    try {
+      await Isolate.spawn(runTask, [receivePort.sendPort, path]);
+    } on Object {
+      print('Isolate Failed');
+      receivePort.close();
     }
-    var cnts =
-        cv.findContours(dilate, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
-    var contours = cnts.$1;
+    final res = await receivePort.first;
+    print('Data: $res');
+    final DateTime endTime = DateTime.now();
+    print("Finished processing at: $endTime");
+  }
 
-    for (var contour in contours) {
-      var rect = cv.boundingRect(contour);
-      _contours.add(
-        Rect(
-          (rect.x / ratio).toInt(),
-          (rect.y / ratio).toInt(),
-          (rect.width / ratio).toInt(),
-          (rect.height / ratio).toInt(),
-        ),
-      );
-    }
+  @override
+  void dispose() {
+    receivePort.close();
+    super.dispose();
   }
 
   @override
@@ -141,37 +225,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(
-              width: imageWidth.toDouble(),
-              height: imageHeight.toDouble(),
-              child: Stack(
-                children: <Widget>[
-                  _image != null
-                      ? Positioned.fill(
-                          child: Image.file(_image!),
-                        )
-                      : Text('No image selected'),
-                  Positioned.fill(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: _contours.map((rect) {
-                        return Positioned(
-                          left: rect.x.toDouble(),
-                          top: rect.y.toDouble(),
-                          width: rect.width.toDouble(),
-                          height: rect.height.toDouble(),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.red, width: 2.0),
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            _image != null ? Image.file(_image!) : Text('No image selected'),
             Card(
               child: _stitchedImage == null
                   ? Placeholder()
